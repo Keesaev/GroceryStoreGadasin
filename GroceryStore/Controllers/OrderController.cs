b4861ff2bad341668224fc9de4cf3f25661b28d2
keesaev <kostia.234@mail.ru>
2021-04-10 22:01:57 +0300

    Админка: панель с заказами

--- GroceryStore/Controllers/OrderController.cs
+++ GroceryStore/Controllers/OrderController.cs
@@ -16,6 +16,20 @@ namespace GroceryStore.Controllers
             repository = repoServise;
             cart = cartService;
         }
+        public ViewResult List() =>
+            View(repository.Orders.Where(o => !o.Shipped));
+        [HttpPost]
+        public IActionResult MarkShipped(int orderId)
+        {
+            Order order = repository.Orders
+                .FirstOrDefault(o => o.Id == orderId);
+            if(order != null)
+            {
+                order.Shipped = true;
+                repository.SaveOrder(order);
+            }
+            return RedirectToAction(nameof(List));
+        }
         public ViewResult Checkout() => View(new Order());
         [HttpPost]
         public IActionResult Checkout(Order order)
